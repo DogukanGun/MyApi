a292bfcfc004485730c5ea73b21683471c8d61bf
Dogukan Ali Gundogan <59707019+DogukanGun@users.noreply.github.com>
2024-07-24 14:14:44 +0200

    Add chat for donation app

--- app/llm.py
+++ app/llm.py
@@ -1,5 +1,10 @@
+from typing import List, Dict, Any
+
 import ollama
 from fastapi import APIRouter, Request
+from ollama import Message
+
+from app.data.llm_data import DonationMessage
 
 router = APIRouter(tags=["LLM"], prefix="/llm")
 
@@ -18,4 +23,19 @@ async def respond_email(
                        '\n' + email,
         },
     ])
+    return response['message']['content']
+
+
+@router.post("/chat/donation")
+async def chat_for_donation(
+        messages: List[DonationMessage]
+):
+    system_message: Dict[str, Any] = {
+        "role": "system",
+        "content": "After this message you will have a user who is trying to create a donation in blockchain. The system has the smart contracts which will be called after the conversation with you. You are trying to get information, user name, and user wallet address to deposit money after the donation and an image. Now answer all questions by talking with the user; do not write as if talking to me. Also, while getting answers, control them. If any field is dummy or the wallet address is not fitting the rules of Ethereum-type wallets, then after all information is taken from the user, tell the user 'thanks, contract will be deployed.' Then return me a message in this format: {\"name\": string, \"wallet\": string, \"reason\": string}"
+    }
+    messages_history: List[Message] = [
+        Message(**system_message) if i == 0 else Message(**messages[i-1].__dict__) for i in
+        range(len(messages) + 1)]
+    response = ollama.chat(model='llama3', messages=messages_history)
     return response['message']['content']
